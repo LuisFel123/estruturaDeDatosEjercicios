ea417122e6146832bb248107703b2f72cee4af34
LuisFel123 <felipe_ramirez1234@hotmail.com>
2026-05-20 13:30:39 -0600

    cambios

---
+++ src/main/java/ColasClaseQueue/Main.java
@@ -0,0 +1,27 @@
+package ColasClaseQueue;
+
+//clase queue
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String[] args){
+        Queue<Integer> colita= new LinkedList<Integer>();
+        colita.add(10);
+        colita.add(20);
+        colita.add(30);
+        colita.add(40);
+        colita.add(50);
+        System.out.println("El incio de la cola es: "+colita.peek());
+
+        int tamanio= colita.size();
+        System.out.println("Vaciando la cola");
+        colita.clear();
+        while (!colita.isEmpty()){
+            System.out.println("Despachando al elemento "+colita.remove());
+        }
+
+
+    }
+}
